fffa35b1769867b36dad74dee18e9fa33324037e
r1ck12 <rickregien@gmail.com>
2023-08-02 01:19:39 -0300

    feat: add Web3Auth connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,89 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+import styles from "./page.module.css"
+import { useEffect, useState } from "react"
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider"
+import { SessionContextValue, useSession } from "next-auth/react"
+import Web3Auth from "@web3auth/single-factor-auth"
+import { CustomSession } from "./api/auth/[...nextauth]/route"
+import Link from "next/link"
+
+const provider = new EthereumPrivateKeyProvider({
+  config: {
+    chainConfig: {
+      chainId: "0x5",
+      rpcTarget: "https://rpc.ankr.com/eth_goerli",
+      displayName: "Goerli Testnet",
+      blockExplorer: "https://goerli.etherscan.io",
+      ticker: "ETH",
+      tickerName: "Ethereum",
+    },
+  },
+})
+
+type SessionContext = Omit<SessionContextValue, "data"> & {
+  data: CustomSession
+}
 
 export default function Home() {
+  const [web3Auth, setWeb3Auth] = useState<Web3Auth | null>(null)
+  const [loggedIn, setLoggedIn] = useState(false)
+  const session = useSession() as SessionContext
+
+  useEffect(() => {
+    const initWeb3Auth = async () => {
+      const web3AuthSfa = new Web3Auth({
+        clientId: "BLicDSGdbxEgzIyECDxPX-uLaVjgdCqwVDMlGeODmhYfJPZbN_X9E2_J9wrYT5-goyT5iPqsKZEawe1opOWUy30",
+        web3AuthNetwork: "testnet",
+        usePnPKey: false,
+      })
+
+      web3AuthSfa.init(provider)
+      if (session.status === "authenticated" && session.data.user && session.data.user.id) {
+        const user = session.data.user
+
+        if (web3AuthSfa.ready) {
+          try {
+            const web3AuthSfaProvider = await web3AuthSfa.connect({
+              verifier: "google-testnet-0101",
+              verifierId: user.email,
+              idToken: user.id,
+            })
+            if (web3AuthSfaProvider) {
+              const ethPrivateKey = await web3AuthSfaProvider.request({ method: "eth_private_key" })
+              console.log(ethPrivateKey)
+            }
+
+            if (web3AuthSfa.sessionId) {
+              console.log("connected")
+              setLoggedIn(true)
+            } else {
+              console.log("not connected")
+            }
+
+            setWeb3Auth(web3AuthSfa)
+          } catch (e) {
+            console.error(e)
+          }
+        }
+      }
+    }
+
+    if (!loggedIn) initWeb3Auth()
+  }, [session])
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
+      <h1>Web3Auth login with Google</h1>
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className={styles.container}>
+          {!loggedIn ? (
+            <Link href="/api/auth/signin">
+              <button>Login</button>
+            </Link>
+          ) : null}
+          {loggedIn ? <button>Logout</button> : null}
+          <button>GetUserInfo</button>
+        </div>
       </div>
     </main>
   )
