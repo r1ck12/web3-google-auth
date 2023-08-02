000bb98fbd2e98455f46d08d3add08cb9bd1de4d
r1ck12 <rickregien@gmail.com>
2023-08-02 01:19:27 -0300

    feat: add custom session and JWT callbacks

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,49 @@
-import NextAuth from "next-auth"
+import NextAuth, { Account, Session, TokenSet, User } from "next-auth"
+import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google"
+
+type SessionBase = Omit<Session, 'user'>;
+
+export type CustomSession = SessionBase & {
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    image: string;
+  }
+};
+
+export type CustomJWT = JWT & {
+  id_token?: string;
+}
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      idToken: true,
     }),
   ],
   callbacks: {
-    session: async ({ session, token }) => {
+    session: async ({ session, token }: { session: Session, token: CustomJWT }): Promise<CustomSession> => {
       if (session?.user) {
         return {
           ...session,
           user: {
             ...session.user,
-            id: token.uid,
-          }
-        }
+            id: token.id_token,
+          },
+        } as CustomSession;
       }
-      return session;
+      return session as CustomSession;
     },
-    jwt: async ({ user, token }) => {
-      if (user) {
-        token.uid = user.id;
+    async jwt({ token, user, account }: { token: JWT, user: User, account: Account | null }): Promise<CustomJWT> {
+      if (account && account.id_token) {
+        return {
+          ...token,
+          id_token: account.id_token,
+        }
       }
       return token;
     },
